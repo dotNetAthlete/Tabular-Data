88d52321b0480c43833ddc9c0078f9eb445a3aa5
Rob Everett <everetthampshire@gmail.com>
2017-06-26 09:07:58 +0100

    Add files via upload
    To-To List written in ASP.NET c#

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ToDoList._Default" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <script src="Scripts/DefaultContent.js"></script>
+    <br />
+        <h1>To Do List</h1>
+    <br />
+    <div>
+        <asp:TextBox ID="txtNewTask" runat="server" BackColor="White" placeholder="New Task...">
+        </asp:TextBox>
+        <asp:Button ID="btnAddTask" runat="server" Text="Add Task" OnClick="btnAddTask_Click"/>
+    </div>
+    <div>
+        <asp:GridView ID="gvTODOList" runat="server" AutoGenerateColumns="false" OnRowDeleting="gvTODOList_RowDeleting"
+            EmptyDataText="No records have been added." BorderStyle="Solid" BorderWidth="1px" CellPadding="4" CellSpacing="2"
+            ShowHeader="false">
+            <Columns>              
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkTaskComplete" runat="server" AutoPostBack="false" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="TaskDescription" />
+                <asp:ButtonField ButtonType="Button" Text="Remove" CommandName="Delete" />
+            </Columns>          
+        </asp:GridView>
+        <br />
+    </div>    
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ToDoList
+{
+    public partial class _Default : Page
+    {
+        bool IsPageRefresh = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) // initial page load - set up GridView datasource
+            {
+                // checks if the page postback is due to genuine submit by user or by pressing "refresh"
+                ViewState["ViewStateId"] = Guid.NewGuid().ToString();
+                Session["SessionId"] = ViewState["ViewStateId"].ToString();            
+
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[2] { new DataColumn("chkTaskComplete"), new DataColumn("TaskDescription") });
+                Session["CurrentTable"] = dt;
+                gvTODOList.DataSource = dt;
+                gvTODOList.DataBind();
+            }
+            else
+            {
+                if (ViewState["ViewStateId"].ToString() != Session["SessionId"].ToString())
+                    IsPageRefresh = true;
+
+                Session["SessionId"] = Guid.NewGuid().ToString();
+                ViewState["ViewStateId"] = Session["SessionId"].ToString();      
+            }
+             txtNewTask.Focus();
+        }
+       
+        protected void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (!IsPageRefresh) // browser refresh...
+            {              
+                Regex pattern = new Regex("(?i)^[a-z]+(?:[ -]?[a-z]+)*$");
+
+                // validate user input
+                if (pattern.IsMatch(txtNewTask.Text.Trim()) == false)
+                    return;
+
+                DataTable dt = (DataTable)Session["CurrentTable"];               
+
+                dt.Rows.Add(false, txtNewTask.Text.Trim());             
+                gvTODOList.DataSource = dt;
+                gvTODOList.DataBind();
+
+                txtNewTask.Text = string.Empty;
+                Session["CurrentTable"] = dt;
+            }                                            
+        }
+      
+        protected void gvTODOList_RowDeleting(object sender, GridViewDeleteEventArgs e)
+         {
+            DataTable dt = (DataTable)Session["CurrentTable"];
+            dt.Rows.RemoveAt(e.RowIndex);                     
+            gvTODOList.DataSource = dt;
+            gvTODOList.DataBind();
+
+            Session["CurrentTable"] = dt;
+        }
+    }
+}
